f23bbe11786ef81e989dd0239e767e4b07ea8a87
Jarrod Sampson <jarrod.sampson91@gmail.com>
2015-09-18 16:27:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+// require express in file
 var express = require('express');
+// initialize
 var app = express();
 
+// set up route
 app.get('/', function (req, res) {
   res.send('Root Directory route.');
 });
 
+// set up as many as you need to
 app.get('/example', function (req, res) {
   res.send('Alternate Route.');
 });
 
+// set server to listen to port 8000
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  // return port number and url for browser
   console.log('Example app listening at http://%s:%s', host, port);
 });
